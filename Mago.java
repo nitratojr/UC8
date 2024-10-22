8b61fd21e2c5db2e90cae1a08625fab25dae9062
nitratojr <162368996+nitratojr@users.noreply.github.com>
2024-10-22 15:26:23 -0300

    Update Mago.java

--- Mago.java
+++ Mago.java
@@ -12,21 +12,4 @@ public class Mago extends Personagem {
     public Mago(String nome){
         super(nome,80,25);
     }
-     public void lancarFeitico(Personagem oponente) {
-        if (this.estarVivo()) {
-            int danoFeitico = this.ataque * 2;
-            oponente.vida -= danoFeitico;
-            System.out.println(this.nome + " lançou um feitiço em " + oponente.nome + " causando " + danoFeitico + " de dano.");
-            if (oponente.vida <= 0) {
-                oponente.vida = 0;
-                System.out.println(oponente.nome + " foi derrotado!");
-            } else {
-                System.out.println("Vida restante de " + oponente.nome + ": " + oponente.vida);
-            }
-        } else {
-            System.out.println(this.nome + " não pode lançar feitiço porque está morto.");
-        }
-        
     
-     }
-}
